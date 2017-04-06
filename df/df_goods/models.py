ee6bfafe4b07e840ed102151c97c975bc0c3b948
shihongguang <2590032200@qq.com>
2017-04-06 11:55:11 +0800

    主页制作完毕

--- df/df_goods/models.py
+++ df/df_goods/models.py
@@ -4,8 +4,6 @@ from tinymce.models import HTMLField
 class TypeInfo(models.Model):
     ttitle=models.CharField(max_length=20)
     isDelete=models.BooleanField(default=False)
-    def __str__(self):
-        return self.ttitle.encode('utf-8')
 
 class GoodsInfo(models.Model):
     gtitle=models.CharField(max_length=20)
@@ -18,5 +16,3 @@ class GoodsInfo(models.Model):
     gkucun=models.IntegerField()
     gcontent=HTMLField()
     gtype=models.ForeignKey(TypeInfo)
-    def __str__(self):
-        return self.gtitle.encode('utf-8')
--- df/df_goods/views.py
+++ df/df_goods/views.py
@@ -1,8 +1,25 @@
+#coding=utf-8
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from models import *
+
+
 
 
 def index(request):
-    return HttpResponse("index")
+    list_type = TypeInfo.objects.order_by("id")
+    
+    list_id_goods = []
+    list_click_goods = []
+
+    for goods_type in list_type:
+        list_id = goods_type.goodsinfo_set.order_by("-id")[:3]
+        list_click = goods_type.goodsinfo_set.order_by("-gclick")[:4] 
+        list_id_goods.append(list_id)
+        list_click_goods.append(list_click)  
+
+    context = {"title":"首页","id":list_id_goods,"click":list_click_goods}
+
+    return render(request, "df_goods/index.html",context)
